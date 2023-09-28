65368eb5719a6204161ff7eef7bf88e120bc30ce
Marguerite Mireille Camara <66399926+MargueriteMireille@users.noreply.github.com>
2023-09-28 13:41:03 -0400

    structure du projet (#1)

---
+++ Tp1/tp1/src/main/java/com/ift3913/App.java
@@ -0,0 +1,13 @@
+package com.ift3913;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
