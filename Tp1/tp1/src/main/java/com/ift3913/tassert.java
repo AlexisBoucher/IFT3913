5d09e6d5a55853f8ee64b7eb0d48a0e09c32dba4
MargueriteMireille <66399926+MargueriteMireille@users.noreply.github.com>
2023-10-03 21:18:55 -0400

    premiere version de tassert

---
+++ Tp1/tp1/src/main/java/com/ift3913/tassert.java
@@ -0,0 +1,40 @@
+package com.ift3913;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class tassert {
+
+
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.exit(1);
+        }
+    
+        String filePath = args[0];
+        int tassert = calcul_tassert(filePath);
+        System.out.println("TASSERT : " + tassert);
+    }
+
+    public static int calcul_tassert(String dir) {
+        int tassert = 0;
+
+        try (BufferedReader line_reader = new BufferedReader(new FileReader(dir))) {
+            String line;
+
+            while ((line=line_reader.readLine())!= null) {
+                line = line.trim();
+                // Vérification: si la ligne contient un appel à une méthode d'assertion JUnit incrementer le tassert
+                if (line.startsWith("assert")|| (line.startsWith("fail"))) {
+                    tassert++;
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Erreur while reading file : " + e.getMessage());
+        }
+        return tassert;
+    }
+
+}
+
