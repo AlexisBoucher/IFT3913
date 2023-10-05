d1d0516fbedd5030b4844aec8d7d6088bbf0da6e
AlexisBoucher <alexis.boucher.2@umontreal.ca>
2023-10-05 12:05:40 -0400

    commentaire

--- Tp1/tp1/src/main/java/com/ift3913/tls.java
+++ Tp1/tp1/src/main/java/com/ift3913/tls.java
@@ -21,7 +21,7 @@ public class tls {
         }
 
         Path path = Paths.get(directory);
-        List<Path> paths = findJavaTestFile(path, ".java");
+        List<Path> paths = findJavaTestFile(path);
         for (Path file: paths){
             String relativePath = file.toString().substring(directory.length());
             int tlocNum = tloc.calcul_tloc(file.toString());
@@ -42,33 +42,47 @@ public class tls {
 
     }
 
-    public static List<Path> findJavaTestFile(Path path, String fileExtension)
-            throws IOException {
+    //Fait une liste des fichiers test java
+    public static List<Path> findJavaTestFile(Path path)
+         throws IOException {
 
-        if (!Files.isDirectory(path)) {
-            throw new IllegalArgumentException("Path must be a directory!");
-        }
+            //Chemin d'acces qui n'est pas a un dossier
+            if (!Files.isDirectory(path)) {
+                throw new IllegalArgumentException("Path must be a directory!");
+            }
 
-        List<Path> result;
-        try (Stream<Path> walk = Files.walk(path)) {
-            result = walk
-                    .filter(Files::isRegularFile)   // is a file
-                    .filter(p -> p.getFileName().toString().endsWith(fileExtension))
-                    .filter(p -> p.getFileName().toString().toLowerCase().contains("test"))
-                    .collect(Collectors.toList());
-        }
-        return result;
+            List<Path> result;
+
+            //regarde chaque fichier dans le dossier et les sous-dossiers et met dans une liste tout les
+            //fichiers java contenant le mot test
+            try (Stream<Path> walk = Files.walk(path)) {
+                result = walk
+                        .filter(Files::isRegularFile)   // is a file
+                        .filter(p -> p.getFileName().toString().endsWith(".java"))
+                        .filter(p -> p.getFileName().toString().toLowerCase().contains("test"))
+                        .collect(Collectors.toList());
+            }
+            return result;
 
     }
 
+    //retourne le nom du paquet d'un fichier base sur la structure d'un projet maven
     public static String getPackage(Path path){
+        //le nom du paquet termine par le nom du dossier contenant le fichier
         Path packageEnd = path.getParent();
+        //le nom du paquet commence par le nom du dossier contenue dans le dossier java
         Path packageStart = packageEnd;
+
+        //trouve le chemin du dossier java contenant le chemin du fichier choisi
         while (!packageStart.getParent().endsWith("java")){
             packageStart = packageStart.getParent();
         }
         packageStart = packageStart.getParent();
+
+        //enleve le chemin du dossier java au chemin du dossier contenant le fichier choisi
         String packageName = packageEnd.toString().substring(packageStart.toString().length()+1);
+
+        //remplace par des point les \ et / selon windows ou mac
         packageName = packageName.replace("\\",".");
         packageName = packageName.replace("/",".");
         return packageName;
