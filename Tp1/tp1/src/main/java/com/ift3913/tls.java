88aea035902e9d5660493a180956fa083790084a
AlexisBoucher <alexis.boucher.2@umontreal.ca>
2023-10-05 20:44:24 -0400

    ajout de la sous classe pour tls

--- Tp1/tp1/src/main/java/com/ift3913/tls.java
+++ Tp1/tp1/src/main/java/com/ift3913/tls.java
@@ -6,13 +6,13 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class tls {
     public static void main(String[] args) throws IOException {
-        String directory ="";
+        String directory = "";
         try {
             directory = args[0];
         } catch (ArrayIndexOutOfBoundsException ex) {
@@ -21,29 +21,21 @@ public class tls {
         }
 
         Path path = Paths.get(directory);
-        List<Path> paths = findJavaTestFile(path);
-        for (Path file: paths){
-            String relativePath = file.toString().substring(directory.length());
-            int tlocNum = tloc.calcul_tloc(file.toString());
-            int tassertNum = tassert.calcul_tassert(file.toString());
-            String tcmpNum;
-            float Tcmp;
-            if(tassertNum!=0){
-                Tcmp = (float)tlocNum/(float) tassertNum;
-                tcmpNum = String.valueOf(Tcmp);}
-            else tcmpNum = "inf";
-            System.out.println(relativePath+","
-                    +getPackage(file)+", "
-                    +file.getFileName()+", "
-                    +tlocNum+", "
-                    +tassertNum+", "
-                    +tcmpNum);
-        }
+        List<tlsFile> tlsFiles = findJavaTestFile(path, directory);
 
+        //imprime les donnees
+        for (tlsFile file: tlsFiles) {
+            System.out.println(file.getRelativePath()+","
+                    +file.getPacket()+", "
+                    +file.getName()+", "
+                    +file.getTloc()+", "
+                    +file.getTassert()+", "
+                    +file.getTcmp());
+        }
     }
 
     //Fait une liste des fichiers test java
-    public static List<Path> findJavaTestFile(Path path)
+    public static List<tlsFile> findJavaTestFile(Path path,String directory)
          throws IOException {
 
             //Chemin d'acces qui n'est pas a un dossier
@@ -51,41 +43,26 @@ public class tls {
                 throw new IllegalArgumentException("Path must be a directory!");
             }
 
-            List<Path> result;
+            List<Path> paths;
+            List<tlsFile> tlsFiles = new ArrayList<>();
 
             //regarde chaque fichier dans le dossier et les sous-dossiers et met dans une liste tout les
             //fichiers java contenant le mot test
             try (Stream<Path> walk = Files.walk(path)) {
-                result = walk
+                paths = walk
                         .filter(Files::isRegularFile)   // is a file
                         .filter(p -> p.getFileName().toString().endsWith(".java"))
                         .filter(p -> p.getFileName().toString().toLowerCase().contains("test"))
                         .collect(Collectors.toList());
             }
-            return result;
 
+            //recueil toute les infos desire
+            for (Path file: paths) {
+                tlsFile tls_File = new tlsFile(file, directory);
+                tlsFiles.add(tls_File);
+            }
+            return tlsFiles;
     }
 
-    //retourne le nom du paquet d'un fichier base sur la structure d'un projet maven
-    public static String getPackage(Path path){
-        //le nom du paquet termine par le nom du dossier contenant le fichier
-        Path packageEnd = path.getParent();
-        //le nom du paquet commence par le nom du dossier contenue dans le dossier java
-        Path packageStart = packageEnd;
-
-        //trouve le chemin du dossier java contenant le chemin du fichier choisi
-        while (!packageStart.getParent().endsWith("java")){
-            packageStart = packageStart.getParent();
-        }
-        packageStart = packageStart.getParent();
-
-        //enleve le chemin du dossier java au chemin du dossier contenant le fichier choisi
-        String packageName = packageEnd.toString().substring(packageStart.toString().length()+1);
-
-        //remplace par des point les \ et / selon windows ou mac
-        packageName = packageName.replace("\\",".");
-        packageName = packageName.replace("/",".");
-        return packageName;
-    }
 
 }
---
+++ Tp1/tp1/src/main/java/com/ift3913/tlsFile.java
@@ -0,0 +1,77 @@
+package com.ift3913;
+
+
+import java.nio.file.Path;
+
+
+public class tlsFile {
+    //info importante
+    protected String relativePath;
+    protected int Tloc;
+    protected int Tassert;
+    protected float Tcmp;
+    protected Path name;
+    protected String packet;
+
+    //constructeur
+    public tlsFile(Path path, String directory) {
+        this.relativePath= path.toString().substring(directory.length());
+        this.name= path.getFileName();
+        this.Tloc= tloc.calcul_tloc(path.toString());
+        this.Tassert= tassert.calcul_tassert(path.toString());
+        if (Tassert!=0){
+            this.Tcmp= (float)Tloc /(float)Tassert;
+        }
+        this.packet= findPackage(path);
+    }
+
+    //retourne le nom du paquet d'un fichier base sur la structure d'un projet maven
+    public static String findPackage(Path path){
+        //le nom du paquet termine par le nom du dossier contenant le fichier
+        Path packageEnd = path.getParent();
+        //le nom du paquet commence par le nom du dossier contenue dans le dossier java
+        Path packageStart = packageEnd;
+
+        //trouve le chemin du dossier java contenant le chemin du fichier choisi
+        while (!packageStart.getParent().endsWith("java")){
+            packageStart = packageStart.getParent();
+        }
+        packageStart = packageStart.getParent();
+
+        //enleve le chemin du dossier java au chemin du dossier contenant le fichier choisi
+        String packageName = packageEnd.toString().substring(packageStart.toString().length()+1);
+
+        //remplace par des point les \ et / selon windows ou mac
+        packageName = packageName.replace("\\",".");
+        packageName = packageName.replace("/",".");
+        return packageName;
+    }
+
+    //getter
+    public String getRelativePath() {
+        return relativePath;
+    }
+
+    public String getTcmp() {
+        if (Tassert!=0){
+            return String.valueOf(Tcmp);
+        }
+        else return "inf";
+    }
+
+    public int getTassert() {
+        return Tassert;
+    }
+
+    public int getTloc() {
+        return Tloc;
+    }
+
+    public Path getName() {
+        return name;
+    }
+
+    public String getPacket() {
+        return packet;
+    }
+}
