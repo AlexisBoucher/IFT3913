bfcdde1b56c1fa8d2ce9efa18261defe4f14e42f
MargueriteMireille <66399926+MargueriteMireille@users.noreply.github.com>
2023-10-27 18:31:48 -0400

    correction tpp

--- Tp2/programmes/tls.java
+++ Tp2/programmes/tls.java
@@ -1,4 +1,3 @@
-//look in jfreechart directory and print all relatives path of java test file
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
@@ -11,89 +10,74 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class tls {
-    private static Map<String, Integer> testsPerPackage = new HashMap<>();
     public static void main(String[] args) throws IOException {
         boolean saveInFile = false;
         String csvFile = "";
         String directory = "";
 
         try {
-            //donne un fichier de sortie
-            if(Objects.equals(args[0], "-o")){
+            // Donne un fichier de sortie
+            if (Objects.equals(args[0], "-o")) {
                 saveInFile = true;
                 csvFile = args[1];
                 directory = args[2];
-            }
-            //aucun fichier de sortie
-            else {
+            } else {
                 directory = args[0];
             }
         } catch (ArrayIndexOutOfBoundsException ex) {
-            System.out.println("One or more argument missing");
+            System.out.println("One or more arguments are missing");
             System.exit(1);
         }
 
-        Map<String, Integer> testsPerPackage = new HashMap<>();
         List<tlsFile> tlsFiles = findJavaTestFile(directory);
 
-        //imprime les donnees
-        saveData(tlsFiles,csvFile,saveInFile);
+        // Imprime les données
+        saveData(tlsFiles, csvFile, saveInFile);
+    }
 
-        // Imprime le nombre de tests par package
-        for (Map.Entry<String, Integer> entry : testsPerPackage.entrySet()) {
-            System.out.println("Package: " + entry.getKey() + ", Nombre de Tests: " + entry.getValue());
+    // Fait une liste des fichiers test Java
+    public static List<tlsFile> findJavaTestFile(String directory) throws IOException {
+        Path path = Paths.get(directory);
+
+        // Chemin d'accès qui n'est pas un dossier
+        if (!Files.isDirectory(path)) {
+            throw new IllegalArgumentException("Path must be a directory!");
         }
+
+        List<Path> paths;
+        List<tlsFile> tlsFiles = new ArrayList<>();
+
+        // Regarde chaque fichier dans le dossier et les sous-dossiers et met dans une liste tous les
+        // fichiers Java contenant le mot "test"
+        try (Stream<Path> walk = Files.walk(path)) {
+            paths = walk
+                    .filter(Files::isRegularFile) // is a file
+                    .filter(p -> p.getFileName().toString().endsWith(".java"))
+                    .filter(p -> p.getFileName().toString().toLowerCase().contains("test"))
+                    .collect(Collectors.toList());
+        }
+
+        // Recueille toutes les informations désirées
+        for (Path file : paths) {
+            tlsFile tlsFile = new tlsFile(file, directory);
+
+            // Vérifie qu'il y ait au moins 1 assert sinon pas considéré comme classe de test
+            if (tlsFile.getTassert() != 0) {
+                tlsFiles.add(tlsFile);
+            }
+        }
+        return tlsFiles;
     }
 
-    //Fait une liste des fichiers test java
-    public static List<tlsFile> findJavaTestFile(String directory)
+    public static void saveData(List<tlsFile> tlsFiles, String csvFile, boolean saveInFile) {
+        // Imprime en ligne de commande
 
-         throws IOException {
-            Path path = Paths.get(directory);
-
-            //Chemin d'acces qui n'est pas a un dossier
-            if (!Files.isDirectory(path)) {
-                throw new IllegalArgumentException("Path must be a directory!");
-            }
-
-            List<Path> paths;
-            List<tlsFile> tlsFiles = new ArrayList<>();
-
-            //regarde chaque fichier dans le dossier et les sous-dossiers et met dans une liste tout les
-            //fichiers java contenant le mot test
-            try (Stream<Path> walk = Files.walk(path)) {
-                paths = walk
-                        .filter(Files::isRegularFile) // is a file
-                        .filter(p -> p.getFileName().toString().endsWith(".java"))
-                        .filter(p -> p.getFileName().toString().toLowerCase().contains("test"))
-                        .collect(Collectors.toList());
-            }
-
-            //recueil toute les infos desire
-            for (Path file: paths) {
-                tlsFile tls_File = new tlsFile(file, directory);
-
-                //verifie qu'il y ait au moins 1 assert sinon pas considere comme classe de test
-                if(tls_File.getTassert()!=0 ){
-                    tlsFiles.add(tls_File);
-
-                // Compte le nombre de tests par package
-                String packageName = tls_File.getPacket();
-                testsPerPackage.put(packageName, testsPerPackage.getOrDefault(packageName, 0) + 1);
-                }
-            }
-            return tlsFiles;
-    }
-
-    public static void saveData(List<tlsFile> tlsFiles, String csvFile, boolean saveInFile){
-        //imprime en ligne de commande
-
-        int tlocGlobal=0;
-        int tassertGlobal=0;
-        Float tcmpGlobal=(float)0;
-        if(!saveInFile){
-            for (tlsFile file: tlsFiles) {
-
+        int tlocGlobal = 0;
+        int tassertGlobal = 0;
+        float tcmpGlobal = 0;
+        int tppGlobal = 0;
+        if (!saveInFile) {
+            for (tlsFile file : tlsFiles) {
                 System.out.println(file.getRelativePath() + ", "
                         + file.getPacket() + ", "
                         + file.getName() + ", "
@@ -102,45 +86,53 @@ public class tls {
                         + file.getTcmp());
                 tlocGlobal += file.getTloc();
                 tassertGlobal += file.getTassert();
-                tcmpGlobal += file.getTcmp();         
+                tcmpGlobal += file.getTcmp();
             }
-            tcmpGlobal = tcmpGlobal/tlsFiles.size();
-            System.out.println("Total, "+tlocGlobal+", "+tassertGlobal+", "+tcmpGlobal);
-        }
-
-        //enregistrer   dans fichier csv
-        else{
-            try{
+            tcmpGlobal = tcmpGlobal / tlsFiles.size();
+            System.out.println("Total, " + tlocGlobal + ", " + tassertGlobal + ", " + tcmpGlobal);
+        } else {
+            try {
                 File file = new File(csvFile);
                 FileWriter output = new FileWriter(file);
                 BufferedWriter writer = new BufferedWriter(output);
-                
+
                 writer.write("Relative Path, Package, File Name, TLOC, TASSERT, TCMP, TPP\n");
-                
-                for(tlsFile tlsFile : tlsFiles){
+
+                Map<String, Integer> testsPerPackage = new HashMap<>();
+
+                for (tlsFile tlsFile : tlsFiles) {
+                    String packageName = tlsFile.getPacket();
+                    int assertionsInClass = tlsFile.getTassert();
+                    testsPerPackage.put(packageName, testsPerPackage.getOrDefault(packageName, 0) + assertionsInClass);
+                }
+
+                for (tlsFile tlsFile : tlsFiles) {
+                    String packageName = tlsFile.getPacket();
+                    int assertionsInPackage = testsPerPackage.get(packageName);
+
                     writer.write(tlsFile.getRelativePath() + ","
                             + tlsFile.getPacket() + ", "
                             + tlsFile.getName() + ", "
                             + tlsFile.getTloc() + ", "
                             + tlsFile.getTassert() + ", "
                             + tlsFile.getTcmp() + ", "
-                            + testsPerPackage.getOrDefault(tlsFile.getPacket(), 0) + "\n");
+                            + assertionsInPackage + "\n");
                     tlocGlobal += tlsFile.getTloc();
                     tassertGlobal += tlsFile.getTassert();
-                    tcmpGlobal += tlsFile.getTcmp();    
+                    tcmpGlobal += tlsFile.getTcmp();
+                    tppGlobal += assertionsInPackage;
+                    
                 }
-                tassertGlobal = tassertGlobal/tlsFiles.size();
-                tcmpGlobal = tcmpGlobal/tlsFiles.size();
-                writer.write("Total, "+tlocGlobal+", "+tassertGlobal+", "+tcmpGlobal);
-               /*  writer.write("\nPackage, Nombre de Tests\n");
-                for (Map.Entry<String, Integer> entry : testsPerPackage.entrySet()) {
-                writer.write(entry.getKey() + ", " + entry.getValue() + "\n");
-                }*/
+                tassertGlobal = tassertGlobal / tlsFiles.size();
+                tcmpGlobal = tcmpGlobal / tlsFiles.size();
+                tppGlobal = tppGlobal/ tlsFiles.size();
+
+                writer.write("Total, " + tlocGlobal + ", " + tassertGlobal + ", " + tcmpGlobal+", "+ tppGlobal+"\n");
+
                 writer.close();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-            catch (Exception e){e.printStackTrace();}
         }
     }
-
-
 }
