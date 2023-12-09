554413f58bb70a8e405939f94ee41572f8799172
AlexisBoucher <alexis.boucher.2@umontreal.ca>
2023-12-08 20:11:37 -0500

    mise en forme

---
+++ Tp4/Alexis_Boucher_Marguerite_Camara/currency-converter-in-java/src/test/PLACEZVOSTESTSICI.md
@@ -0,0 +1,11 @@
+Places vos tests ici. Utilisation de JUnit5 requise.
+
+
+
+Tests: 
+testBoiteNoire
+    MainWindowTestBoiteNoire.java
+    CurrencyTestBoiteNoire
+testBoiteBlanche
+    MainWindowTestBoiteBlanche.java
+    CurrencyTestBoiteBlanche.java
---
+++ Tp4/Alexis_Boucher_Marguerite_Camara/currency-converter-in-java/src/test/testBoiteNoire/MainWindowTestBoiteNoire.java
@@ -0,0 +1,138 @@
+package testBoiteNoire;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import currencyConverter.Currency;
+import currencyConverter.MainWindow;
+import java.util.ArrayList;
+
+import org.junit.Before;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.text.ParseException;
+
+// Exception retourné pour un montant < 0 
+class NegativeNumberException extends Exception {
+	String number;
+	NegativeNumberException(String n1){
+		number = n1;
+	}
+}
+//Exception retourné pour des montant > 1 000 000
+class OverLimitException extends Exception {
+	String m;
+	OverLimitException(String m1){
+		m = m1;
+	}	
+}
+
+public class MainWindowTestBoiteNoire {
+
+   private ArrayList<Currency> currencies;
+
+    @Before
+    public void setUp() {
+        currencies = Currency.init();
+    }
+
+    public void NegativeException(double n) throws NegativeNumberException {
+			
+		if(n < 0) {
+			throw new NegativeNumberException("Negative amount");
+		}
+    }
+    public void OverLimitExceptionValue(double n) throws OverLimitException {
+		if(n > 1000000) {
+			throw new OverLimitException("Amount is over 1 000 000");
+		}
+	}
+    //Boite Noire ----------------------------------
+   
+    //Test d un montant négatif
+    @Test(expected = NegativeNumberException.class)
+	public void convert_neg_ammount_correct_devise() throws ParseException, NegativeNumberException {
+        double amount = -10.0 ;
+	    double convert = MainWindow.convert("US Dollar", "Euro",currencies, amount);
+        System.out.println(currencies.get(0).getName());
+        assertTrue( amount < 0.0);
+	    NegativeException(-10.0);
+	}
+    //Test d'un montant superieur à 1 000 000
+    @Test(expected = OverLimitException.class)
+	public void Test_USDtoCAD_frontiere_over_10000() throws ParseException, OverLimitException  {
+	        double convert = MainWindow.convert("US Dollar", "Euro", currencies, 1000060.0);
+	        OverLimitExceptionValue(1000060.0);
+	}
+
+    //Test sur des devises faisant partir de la specification
+    @Test
+    public void testConvertUSDtoEUR() {
+        double amount = 0;
+        double converted = MainWindow.convert("US Dollar", "Euro", currencies, amount);
+         //Taux de chang reel (sans considerer celui du converter)
+        double expectedEURRate = 0.93;
+        double expectedUSDRate = 1.073;
+
+        assertTrue("Amount should be between 0 and 1000000",amount >= 0 && amount <= 1000000);
+        double expectedAmount = amount * (expectedEURRate / expectedUSDRate);
+        assertEquals(expectedAmount, converted, 0.001);
+    }
+
+
+    // Montant = 250000
+    @Test
+    public void testConvertValidAmount() {
+        double  amount = 250000.0;
+        double result = MainWindow.convert("US Dollar", "Euro", currencies, amount);
+        //Taux de change reel (sans considerer celui du converter)
+        double expectedEURRate = 0.93;
+        double expectedAmount = amount * (expectedEURRate);
+        assertEquals(expectedAmount, result, 0.001);
+    }
+
+    // Autre devise de la specification
+    @Test
+    public void testConvertOtherCurrency() {
+        double expectedCADRate = 0.89;
+        double  amount = 500.0;
+        double result = MainWindow.convert("Australian Dollar", "Canadian Dollar", currencies, amount);
+        double expectedAmount = amount * (expectedCADRate);
+        assertEquals(expectedAmount, result, 0.001);
+    }
+    //Devise invalide de la specification
+    @Test
+    public void testConvertInvalidCurrency() {
+        double  amount = 500.0;
+        double result = MainWindow.convert("US Dollar", "Guinean Franc", currencies, amount);
+        assertEquals(0, result, 0.001);
+    }
+    // Analyse des valeurs frontières
+    @Test
+    public void testConvertZeroAmount() {
+        double amount = MainWindow.convert("US Dollar", "Euro", currencies, 0.0);
+        assertEquals(0.0, amount, 0.001);
+    }
+
+    @Test
+    public void testConvertMaxAmount() {
+         //Taux de change reel (sans considerer celui du converter)
+        double expectedEURRate = 1.05;
+        double amount = 1000000.0;
+        double convert = MainWindow.convert("Swiss Franc", "Euro", currencies, amount);
+        double expectedAmount = amount * (expectedEURRate);
+        assertEquals(expectedAmount, convert, 0.001);
+    }
+    
+
+
+
+
+
+
+
+
+
+    
+}
